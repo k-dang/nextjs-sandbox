58009c028b14824dc3b40c2dfeec8d9e4f2f1edf
Kevin Dang <dangkevin12@gmail.com>
2025-10-25 16:12:57 -0400

    dynamic nav links

--- src/components/nav-breadcrumb.tsx
+++ src/components/nav-breadcrumb.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Breadcrumb,
   BreadcrumbList,
@@ -6,18 +8,43 @@ import {
   BreadcrumbSeparator,
   BreadcrumbPage,
 } from "@/components/ui/breadcrumb";
+import { navData, NavSection } from "@/lib/navigation";
+import { usePathname } from "next/navigation";
+
+function findBreadCrumb(navMain: NavSection[], path: string) {
+  for (const nav of navMain) {
+    if (nav.url === path) {
+      return { parent: nav.title, child: null };
+    }
+
+    for (const item of nav.items || []) {
+      if (item.url === path) {
+        return { parent: nav.title, child: item.title };
+      }
+    }
+  }
+
+  return {};
+}
 
 export function NavBreadcrumb() {
+  const pathname = usePathname();
+  const { parent, child } = findBreadCrumb(navData.navMain, pathname);
+
   return (
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem className="hidden md:block">
-          <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+          <BreadcrumbLink href="/dashboard">{parent}</BreadcrumbLink>
         </BreadcrumbItem>
-        <BreadcrumbSeparator className="hidden md:block" />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Static Rendering</BreadcrumbPage>
-        </BreadcrumbItem>
+        {child != null && (
+          <>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{child}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </>
+        )}
       </BreadcrumbList>
     </Breadcrumb>
   );
