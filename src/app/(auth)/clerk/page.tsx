ccfdb2397694e79ff56168905199fe9ca8a77303
Kevin Dang <dangkevin12@gmail.com>
2025-10-26 14:52:10 -0400

    add clerk

--- src/app/(auth)/clerk/page.tsx
+++ src/app/(auth)/clerk/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Page() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl">Clerk</h1>
+      <SignedOut>
+        <h2>Sign In or Sign Up</h2>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+        <SignUpButton>
+          <Button>Sign Up</Button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <h2>You are signed in as</h2>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
