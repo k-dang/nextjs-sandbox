0c78650c9a916fb1a29c7d5a9389bfb491086a54
Kevin Dang <dangkevin12@gmail.com>
2025-10-26 15:16:37 -0400

    fix linting errors

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { Search, X } from "lucide-react";
+import { Search } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
