7f23275d4308835589dc9efb89313119312f1d53
Kevin Dang <dangkevin12@gmail.com>
2025-11-02 20:21:50 -0500

    remove image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl">Sandbox</h1>
       <p>This is a sandbox for testing and learning Next.js features.</p>
-      <Image
-        className="dark:invert"
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        priority
-      />
     </div>
   );
 }
